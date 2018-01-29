214c6903482e8e621e09966da89f4a0038fe3ff3
Ezia <esia.belbachir@protonmail.com>
2018-01-29 15:22:48 -0500

    Implement multiple simulations and stats

--- games.py
+++ games.py
@@ -2,6 +2,7 @@
 
 from abc import ABCMeta
 from card_dealer import * 
+import math
 
 def playGame1(cardDealer):
     card = cardDealer.draw_card()
@@ -48,24 +49,55 @@ def playGame5(cardDealer):
 
     return maxSeriesLength >= 3
 
+# simulation parameter values
+
 DEALER = CardDealer()
 ITERATION_NBR = 1000000
+SIMULATION_NBR = 5
 
-game1Wins = 0
-game2Wins = 0
-game3Wins = 0
-game4Wins = 0
-game5Wins = 0
-for i in iter(range(ITERATION_NBR)):
-    if (i % 10000 == 0):
-        print('iteration ' + str(i))
-    game1Wins += 1 if playGame1(DEALER) else 0
-    game2Wins += 1 if playGame2(DEALER) else 0
-    game3Wins += 1 if playGame3(DEALER) else 0
-    game4Wins += 1 if playGame4(DEALER) else 0
-    game5Wins += 1 if playGame5(DEALER) else 0
-print('game 1 wins = ' + str(game1Wins) + ' -> ' + str(100.*game1Wins / ITERATION_NBR) + '%')
-print('game 2 wins = ' + str(game2Wins) + ' -> ' + str(100.*game2Wins / ITERATION_NBR) + '%')
-print('game 3 wins = ' + str(game3Wins) + ' -> ' + str(100.*game3Wins / ITERATION_NBR) + '%')
-print('game 4 wins = ' + str(game4Wins) + ' -> ' + str(100.*game4Wins / ITERATION_NBR) + '%')
-print('game 5 wins = ' + str(game5Wins) + ' -> ' + str(100.*game5Wins / ITERATION_NBR) + '%')
+# simulation
+
+# gameWinsData[i][j] = succes number for j-th game at i-th simulation
+gameWinsData = []
+
+for i in range(SIMULATION_NBR):
+    print('----- Simulation number ' + str(i+1) + '/' + str(SIMULATION_NBR))
+    gameWins = [0] * 5
+    for i in iter(range(ITERATION_NBR)):
+        if (i % 10000 == 0):
+            print('execution ... ' + str(100. * i / ITERATION_NBR) + '%', end="\r")
+        gameWins[0] += 1 if playGame1(DEALER) else 0
+        gameWins[1] += 1 if playGame2(DEALER) else 0
+        gameWins[2] += 1 if playGame3(DEALER) else 0
+        gameWins[3] += 1 if playGame4(DEALER) else 0
+        gameWins[4] += 1 if playGame5(DEALER) else 0
+    gameWinsData.append(gameWins)
+    for j in range(5):
+        print('game' + str(j+1) + ' : wins = ' + str(gameWins[j]) + ' -> ' + str(100.*gameWins[j] / ITERATION_NBR) + '%')
+
+# final stats
+
+print('----- Final stats')
+
+# compute average
+
+gameWinsAverage = [0] * 5
+for i in range(SIMULATION_NBR):
+    for j in range(5):
+        gameWinsAverage[j] += gameWinsData[i][j]
+for i in range(5):
+    gameWinsAverage[i] /= float(SIMULATION_NBR)
+
+# compute standard deviation
+
+gameWinsStdDev = [0] * 5
+for i in range(SIMULATION_NBR):
+    for j in range(5):
+        gameWinsStdDev[j] += pow(gameWinsData[i][j] - gameWinsAverage[j], 2)
+for i in range(5):
+    gameWinsStdDev[i] = math.sqrt(gameWinsStdDev[i] / float(SIMULATION_NBR))
+
+# print result
+
+for i in range(5):
+    print('game' + str(i+1) + ' : avg wins = ' + str(100. * gameWinsAverage[i] / ITERATION_NBR) + '%' + ' ---- std dev wins = ' + str(100. * gameWinsStdDev[i] / ITERATION_NBR) + '%')
